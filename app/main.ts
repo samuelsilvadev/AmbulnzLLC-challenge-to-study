768ae75925f6c16f895be107ab066bd919f1f795
Samuel SIlva <samuelsilvawb@gmail.com>
2018-03-07 21:56:55 -0300

    chore : create files to run app
    - created config file to systemjs
    - created tsconfig
    - created app component

---
+++ app/main.ts
@@ -0,0 +1,5 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+const plataform = platformBrowserDynamic();
+plataform.bootstrapModule(AppModule);
