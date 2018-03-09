319c148cce73dba352b97043c5d6f512b9f52be4
Samuel SIlva <samuelsilvawb@gmail.com>
2018-03-08 21:48:10 -0300

    feat : add cors in server
    - added statements to allow access in server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,13 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+});
+
 app.get('/pizzas/', (req, res) => {
     const pizzasPath = path.join(__dirname, 'pizzas.json');
     const readable = fs.createReadStream(pizzasPath);
