9fadc113e299fa8197387c8fa3e5897877c7892f
Samuel SIlva <samuelsilvawb@gmail.com>
2018-03-12 18:42:52 -0300

    feat : create message after make order
    - create component to show message of time

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
-import { AppService } from './app.service';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+
+import { AppService } from './app.service';
 
 @Component({
     selector: 'app',
@@ -11,16 +12,29 @@ export class AppComponent {
 
     listOrders: Array<any> = [];
     totalPrice = 0;
+    totalTime = 0;
     pizzas$: Observable<any>;
+    textOrder: any;
 
     constructor(private _service: AppService) {
         this.pizzas$ = this._service.getAllPizzas();
     }
 
     order(pizza: any) {
-        this._service.makeOrder().subscribe(data => {
-            this.totalPrice += pizza.price;
-            this.listOrders.push(pizza);
+        this._service.makeOrder().subscribe((data: any) => {
+            console.log(data);
+            if (data.success) {
+                this.totalTime += data.deliveryTime;
+                this.totalPrice += pizza.price;
+                this.listOrders.push(pizza);
+                this.textOrder = `Your order will be delivered in ${this._millisToMinutesAndSeconds(this.totalTime)} minutes`;
+            }
         })
+    }
+
+    private _millisToMinutesAndSeconds(millis: number) {
+        const minutes = Math.floor(millis / 60000);
+        const seconds = ((millis % 60000) / 1000).toFixed(0);
+        return minutes + ":" + (Number(seconds) < 10 ? '0' : '') + seconds;
     }
 }
